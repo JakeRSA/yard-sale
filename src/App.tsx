75e7e3521a9cf3d1ca457daf9a0d4ce1eca98a13
JakeRSA <jaken@wix.com>
2023-04-02 15:50:34 +0300

    config for eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import React from 'react';
-import { Box, Container, CssBaseline, Grid, Paper, Typography } from '@mui/material';
+import { Box, Container, CssBaseline, Grid, Typography } from '@mui/material';
 import { SaleItem, items } from './demoData';
 import { SaleItemCard } from './InstaCarousel/Carousel';
 
 function App() {
+	const renderItemCard = (item: SaleItem) => {
+		return (
+			<Grid item xs={12} xl={3}>
+				<SaleItemCard item={item} />
+			</Grid>
+		);
+	};
 
-const renderItemCard = (item: SaleItem) => {
-
-return (
-  <Grid item xs={12} xl={3}>
-  <SaleItemCard item={item}/>
-  </Grid>
-)
-}
-
-  return (
-    <CssBaseline>
-      <Container>
-        <Typography component={'div'}>
-        <Box sx={{textAlign: 'center'}}>
-        Tal & Jake's Yard Sale
-        </Box>
-        </Typography>
-<Grid container spacing={2}>
-
-        {items.map((item) => renderItemCard(item))}
-</Grid>
-
-      </Container>
-      </CssBaseline>
-  );
+	return (
+		<CssBaseline>
+			<Container>
+				<Typography component={'div'}>
+					<Box sx={{ textAlign: 'center' }}>{`Tal & Jake's Yard Sale`}</Box>
+				</Typography>
+				<Grid container spacing={2}>
+					{items.map((item) => renderItemCard(item))}
+				</Grid>
+			</Container>
+		</CssBaseline>
+	);
 }
 
 export default App;
--- src/InstaCarousel/Carousel.tsx
+++ src/InstaCarousel/Carousel.tsx
@@ -1,88 +1,96 @@
-import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { makeStyles } from 'tss-react/mui'
-import { Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, IconButton, Typography, SxProps,  } from '@mui/material'
-import { Favorite, Share, MoreVert, Comment } from '@mui/icons-material'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { makeStyles } from 'tss-react/mui';
+import {
+	Card,
+	CardHeader,
+	CardMedia,
+	CardContent,
+	CardActions,
+	IconButton,
+	Typography,
+	SxProps,
+} from '@mui/material';
+import { Favorite, Share, Comment } from '@mui/icons-material';
 import SwiperCore, {
-    Keyboard,
-    Scrollbar,
-    Pagination,
-    Navigation,
-} from 'swiper'
+	Keyboard,
+	Scrollbar,
+	Pagination,
+	Navigation,
+} from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import { SaleItem, items } from '../demoData'
+import { SaleItem } from '../demoData';
 const useStyles = makeStyles()({
-    media: {
-        height: 0,
-        paddingTop: '100%',
-    },
-    swiperContainer: {
-        paddingBottom: '3rem',
-        '& .swiper-pagination-bullet': {
-            background: 'blue',
-        },
-        '& .swiper-button-next:after': {
-            fontSize: '2rem !important',
-        },
-        '& .swiper-button-prev:after': {
-            fontSize: '2rem !important',
-        },
-    },
-})
-SwiperCore.use([Keyboard, Scrollbar, Pagination, Navigation])
+	media: {
+		height: 0,
+		paddingTop: '100%',
+	},
+	swiperContainer: {
+		paddingBottom: '3rem',
+		'& .swiper-pagination-bullet': {
+			background: 'blue',
+		},
+		'& .swiper-button-next:after': {
+			fontSize: '2rem !important',
+		},
+		'& .swiper-button-prev:after': {
+			fontSize: '2rem !important',
+		},
+	},
+});
+SwiperCore.use([Keyboard, Scrollbar, Pagination, Navigation]);
 
 interface ISaleItemCardProps {
-    item: SaleItem;
-    sx?: SxProps
+	item: SaleItem;
+	sx?: SxProps;
 }
 
-export const SaleItemCard = ({item, sx}: ISaleItemCardProps) => {
-    const { title, description, images} = item
+export const SaleItemCard = ({ item, sx }: ISaleItemCardProps) => {
+	const { title, description, images } = item;
 
-    const { classes } = useStyles()
-    return (
-        <Card sx={sx}>
-            <CardHeader
-                title={title}
-                // action={
-                //     <IconButton>
-                //         <MoreVert />
-                //     </IconButton>
-                // }
-            />
-            <Swiper
-                grabCursor
-                keyboard={{ enabled: true }}
-                pagination={{ clickable: true }}
-                navigation
-                loop
-                className={classes.swiperContainer}
-            >
-                {images.map((image, index) => (
-                    <SwiperSlide key={index}>
-                        <CardMedia className={classes.media} image={image.src} />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-            <CardActions disableSpacing>
-                <IconButton>
-                    <Favorite />
-                </IconButton>
-                <IconButton>
-                    <Comment />
-                </IconButton>
-                <IconButton>
-                    <Share />
-                </IconButton>
-            </CardActions>
-            <CardContent>
-                <Typography variant='body2' color='textSecondary' component='p'>
-                    {description}
-                </Typography>
-            </CardContent>
-        </Card>
-    )
-}
+	const { classes } = useStyles();
+	return (
+		<Card sx={sx}>
+			<CardHeader
+				title={title}
+				// action={
+				//     <IconButton>
+				//         <MoreVert />
+				//     </IconButton>
+				// }
+			/>
+			<Swiper
+				grabCursor
+				keyboard={{ enabled: true }}
+				pagination={{ clickable: true }}
+				navigation
+				loop
+				className={classes.swiperContainer}
+			>
+				{images.map((image, index) => (
+					<SwiperSlide key={index}>
+						<CardMedia className={classes.media} image={image.src} />
+					</SwiperSlide>
+				))}
+			</Swiper>
+			<CardActions disableSpacing>
+				<IconButton>
+					<Favorite />
+				</IconButton>
+				<IconButton>
+					<Comment />
+				</IconButton>
+				<IconButton>
+					<Share />
+				</IconButton>
+			</CardActions>
+			<CardContent>
+				<Typography variant="body2" color="textSecondary" component="p">
+					{description}
+				</Typography>
+			</CardContent>
+		</Card>
+	);
+};
