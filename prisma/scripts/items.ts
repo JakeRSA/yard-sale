0023de25c17a0b0522d8ecc13608b4bcc5b06a09
JakeRSA <jaken@wix.com>
2023-04-08 19:25:45 +0300

    update mock data

--- prisma/scripts/items.ts
+++ prisma/scripts/items.ts
@@ -1,30 +1,31 @@
 export const items = [
 	{
-		title: 'tv',
-		description: 'Samsung 55 inch',
+		title: 'Oven (Breville Smart Oven Pro)',
+		description:
+			'Excellent condition - includes original packaging and warranty',
 		x: 0,
 		y: 0,
 		z: 0,
 		price: 1800,
 		images: [
 			{
-				src: 'https://filedn.com/ly26l0AIs1CYrO29uHy9zDm/Yard%20Sale/Oven/81KRhLp0JGL._SL1500_.jpg',
+				src: 'https://filedn.com/ly26l0AIs1CYrO29uHy9zDm/Yard%20Sale/Oven%20%28Breville%20Smart%20Oven%20Pro%29/2023-04-05%2010-44-31.jpeg',
 			},
 			{
-				src: 'https://filedn.com/ly26l0AIs1CYrO29uHy9zDm/Yard%20Sale/Oven/91NEQwFKwkL._SL1500_.jpg',
+				src: 'https://filedn.com/ly26l0AIs1CYrO29uHy9zDm/Yard%20Sale/Oven%20%28Breville%20Smart%20Oven%20Pro%29/2023-04-05%2010-45-14.jpeg',
 			},
 		],
 	},
 	{
-		title: 'oven',
-		description: 'Breville Smart Oven',
+		title: 'TV (Samsung UE55AU7100 55" 4k)',
+		description: 'Excellent condition - with warranty',
 		x: 0,
 		y: 0,
 		z: 0,
 		price: 800,
 		images: [
 			{
-				src: 'https://filedn.com/ly26l0AIs1CYrO29uHy9zDm/Yard%20Sale/TV/placesandspaces.jpeg',
+				src: 'https://filedn.com/ly26l0AIs1CYrO29uHy9zDm/Yard%20Sale/TV%20%28Samsung%20UE55AU7100%20-%2055%20inch%204k%29/2023-04-05%2012-15-56.jpeg',
 			},
 		],
 	},
